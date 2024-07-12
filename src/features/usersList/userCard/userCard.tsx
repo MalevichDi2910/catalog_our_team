58d1fd46999018b1af3ddb7c9611948251efde81
MalevichDi2910 <malevicdiana@gmail.com>
2024-07-12 18:55:03 +0300

    feat: add UserCard

---
+++ src/features/usersList/userCard/userCard.tsx
@@ -0,0 +1,34 @@
+import s from "features/usersList/userCard/userCard.module.scss";
+import { Like } from "assets/icons/like";
+import { UserType } from "features/usersList/api/users.types";
+import { Link } from "react-router-dom";
+import { useAppDispatch } from "common/store";
+import { toggleLike } from "features/usersList/model/usersReducer";
+
+type UserCardPropsType = {
+  user: UserType;
+};
+export const UserCard = ({ user }: UserCardPropsType) => {
+  const dispatch = useAppDispatch();
+  const handleLike = () => {
+    dispatch(toggleLike(user.id));
+  };
+
+  return (
+    <div className={s.card}>
+      <Link to={`/users/${user.id}`} className={s.link}>
+        <img src={user.avatar} alt={`${user.first_name} ${user.last_name}`} />
+        <span className={s.info}>
+          {user.first_name} {user.last_name}
+        </span>
+      </Link>
+      <span className={s.like} onClick={handleLike}>
+        <Like
+          fill={
+            user.like ? "var(--color-primary-300)" : "var(--color-dark-100)"
+          }
+        />
+      </span>
+    </div>
+  );
+};
