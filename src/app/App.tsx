505a4d51de0de57cc73312aa5b90b0aaf3a0076d
MalevichDi2910 <malevicdiana@gmail.com>
2024-07-12 16:24:27 +0300

    feat: add useAuthInitialization

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { Router } from "common/router/router";
+import { useAuthInitialization } from "common/authInitialization/authInitialization";
+import s from "./App.module.css";
 
 function App() {
+  useAuthInitialization();
   return (
-    <>
+    <div className={s.app}>
       <Router />
-    </>
+    </div>
   );
 }
 
