c77dc8a236be5d4d0ae0990d57a267d2c4844ba9
MalevichDi2910 <malevicdiana@gmail.com>
2024-07-12 18:51:36 +0300

    feat: add notFoundPage

---
+++ src/components/notFoundPage/notFoundPage.tsx
@@ -0,0 +1,28 @@
+import { NotFound } from "assets/icons/notFound";
+
+import s from "./notFoundPage.module.scss";
+import { Button } from "components/button/button";
+import { useNavigate } from "react-router-dom";
+
+export const NotFoundPage = () => {
+  const navigate = useNavigate();
+
+  const goBack = () => {
+    navigate("/users");
+  };
+
+  return (
+    <div className={s.container}>
+      <div className={s.image}>
+        <NotFound />
+      </div>
+      <span className={s.text}>Sorry! Page not found!</span>
+      <Button
+        name={"Back to home page"}
+        variant={"primary"}
+        onClick={goBack}
+        className={s.btn}
+      />
+    </div>
+  );
+};
