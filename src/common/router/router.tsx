fab267bd96645ffb0dc556d8d48d3af37726c389
MalevichDi2910 <malevicdiana@gmail.com>
2024-07-12 21:13:09 +0300

    feat: add createHashRouter

--- src/common/router/router.tsx
+++ src/common/router/router.tsx
@@ -1,33 +1,22 @@
-import {
-  Navigate,
-  RouterProvider,
-  createBrowserRouter,
-} from "react-router-dom";
+import { Navigate, RouterProvider, createHashRouter } from "react-router-dom";
 
 import { Layout } from "./../layout/layout";
 import { PrivateRoutes } from "./privateRoutes";
 import { privateRoutes, publicRoutes } from "./settings";
 
-const basename = "/our_team/";
-
-const router = createBrowserRouter(
-  [
-    {
-      children: [
-        {
-          children: privateRoutes,
-          element: <PrivateRoutes />,
-        },
-        ...publicRoutes,
-      ],
-      element: <Layout />,
-      errorElement: <Navigate to={"/not-found"} />,
-    },
-  ],
+const router = createHashRouter([
   {
-    basename: basename,
+    children: [
+      {
+        children: privateRoutes,
+        element: <PrivateRoutes />,
+      },
+      ...publicRoutes,
+    ],
+    element: <Layout />,
+    errorElement: <Navigate to={"/not-found"} />,
   },
-);
+]);
 
 export const Router = () => {
   return <RouterProvider router={router} />;
