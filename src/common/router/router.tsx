3c462afffbb955ef8e0f10082b98fb091000712c
MalevichDi2910 <malevicdiana@gmail.com>
2024-07-12 19:58:56 +0300

    feat: add basename

--- src/common/router/router.tsx
+++ src/common/router/router.tsx
@@ -8,19 +8,26 @@ import { Layout } from "./../layout/layout";
 import { PrivateRoutes } from "./privateRoutes";
 import { privateRoutes, publicRoutes } from "./settings";
 
-const router = createBrowserRouter([
+const basename = "our_team/";
+
+const router = createBrowserRouter(
+  [
+    {
+      children: [
+        {
+          children: privateRoutes,
+          element: <PrivateRoutes />,
+        },
+        ...publicRoutes,
+      ],
+      element: <Layout />,
+      errorElement: <Navigate to={"/not-found"} />,
+    },
+  ],
   {
-    children: [
-      {
-        children: privateRoutes,
-        element: <PrivateRoutes />,
-      },
-      ...publicRoutes,
-    ],
-    element: <Layout />,
-    errorElement: <Navigate to={"/not-found"} />,
+    basename: basename,
   },
-]);
+);
 
 export const Router = () => {
   return <RouterProvider router={router} />;
