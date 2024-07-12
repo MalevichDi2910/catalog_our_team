f31b2173af2f8241b50f34f67414d0f3c8ae3f9f
MalevichDi2910 <malevicdiana@gmail.com>
2024-07-12 18:53:13 +0300

    feat: add ControlledTextField

---
+++ src/components/controlled/controlledTextField.tsx
@@ -0,0 +1,30 @@
+import {
+  FieldValues,
+  UseControllerProps,
+  useController,
+} from "react-hook-form";
+
+import { TextField, TextFieldProps } from "./../textField/textField";
+
+export type ControlledTextFieldProps<T extends FieldValues> = Pick<
+  UseControllerProps<T>,
+  "control" | "name"
+> &
+  Omit<TextFieldProps, "name" | "onChangeValue">;
+
+export const ControlledTextField = <T extends FieldValues>({
+  control,
+  name,
+  ...rest
+}: ControlledTextFieldProps<T>) => {
+  const {
+    field: { onChange, value, ...field },
+  } = useController({
+    control,
+    name,
+  });
+
+  return (
+    <TextField {...rest} {...field} onChangeValue={onChange} value={value} />
+  );
+};
